8bff1b3746c4a0a16f4f172c4cc404fae2524d59
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-11-01 19:06:19 -0700

    adding .psm1 file

---
+++ publish-module.psm1
@@ -0,0 +1,133 @@
+[cmdletbinding()]
+param()
+
+function AspNet-Publish{
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory = $true,Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        $PublishProperties,
+        [Parameter(Mandatory = $true,Position=1,ValueFromPipelineByPropertyName=$true)]
+        $OutputPath
+    )
+    process{
+        $pubMethod = $PublishProperties['WebPublishMethod']
+        'pubMethod: {0}' -f $pubMethod | Write-Host -ForegroundColor Green
+        # figure out which of the impl method to call for the specific publish method
+        switch ($pubMethod){
+            'MSDeploy' {AspNet-PublishMSDeploy -PublishProperties $PublishProperties -OutputPath $OutputPath}
+            'FileSystem' {AspNet-PublishMSDeploy -PublishProperties $PublishProperties -OutputPath $OutputPath}
+            default { throw ('Unknown value for WebPublishMethod [{0}]' -f $pubMethod)}
+        }
+    }
+}
+
+function AspNet-PublishMSDeploy{
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory = $true,Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        $PublishProperties,
+        [Parameter(Mandatory = $true,Position=1,ValueFromPipelineByPropertyName=$true)]
+        $OutputPath
+    )
+    process{
+        'Inside AspNet-PublishMSDeploy' | Write-Host -ForegroundColor Green
+        # call msdeploy.exe to start the publish process
+
+        if($PublishProperties){
+            # TODO: Get passwod from $PublishProperties
+            $publishPwd = $env:PublishPassword
+            if(!$publishPwd){
+                throw 'Publish password is not found please set $env:PublishPassword'
+            }
+
+            <#
+            "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe" 
+                -source:IisApp='C:\Users\vramak\AppData\Local\Temp\AspNetPublish\WebApplication184-93\wwwroot' 
+                -dest:IisApp='vramak4',ComputerName='https://vramak4.scm.azurewebsites.net/msdeploy.axd',UserName='$vramak4',Password='<PWD>',IncludeAcls='False',AuthType='Basic' 
+                -verb:sync 
+                -enableRule:DoNotDeleteRule 
+                -enableLink:contentLibExtension 
+                -retryAttempts=2 
+                -userAgent="VS14.0:PublishDialog:WTE14.0.51027.0"
+            #>
+            # TODO: Get wwwroot value from $PublishProperties
+            $webrootOutputFolder = (get-item (Join-Path $OutputPath 'wwwroot')).FullName
+            $publishArgs = @()
+            $publishArgs += ('-source:IisApp=''{0}''' -f "$webrootOutputFolder")
+            $publishArgs += ('-dest:IisApp=''{0}'',ComputerName=''{1}'',UserName=''{2}'',Password=''{3}'',IncludeAcls=''False'',AuthType=''Basic''' -f 
+                                    $PublishProperties['DeployIisAppPath'],
+                                    (Get-MSDeployFullUrlFor -msdeployServiceUrl $PublishProperties['MSDeployServiceURL']),
+                                    $PublishProperties['UserName'],
+                                    $publishPwd)
+            $publishArgs += '-verb:sync'
+            # TODO: Should we pass this property in?
+            $publishArgs += '-enableRule:DoNotDeleteRule'
+            $publishArgs += '-enableLink:contentLibExtension'
+            $publishArgs += '-retryAttempts=2'
+
+            'Calling msdeploy with the call {0}' -f (($publishArgs -join ' ').Replace($publishPwd,'{PASSWORD}')) | Write-Verbose
+            & (Get-MSDeploy) $publishArgs
+        }
+        else{
+            throw 'PublishProperties is empty, cannot publish'
+        }
+    }
+}
+
+function AspNet-PublishFileSystem{
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory = $true,Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        $PublishProperties,
+        [Parameter(Mandatory = $true,Position=1,ValueFromPipelineByPropertyName=$true)]
+        $OutputPath
+    )
+    process{
+        # do a file system copy, if there is any skips we have to take care of it in an 
+    }
+}
+
+function Get-MSDeploy {
+    [cmdletbinding()]
+    param()
+    process{
+        $msdInstallLoc = $env:MSDeployPath
+        if(!($msdInstallLoc)) {
+        # TODO: Get this from HKLM SOFTWARE\Microsoft\IIS Extensions\MSDeploy See MSDeploy VS task for implementation
+            $progFilesFolder = (Get-ChildItem env:"ProgramFiles").Value
+            $msdLocToCheck = @()
+            $msdLocToCheck += ("{0}\IIS\Microsoft Web Deploy V3\msdeploy.exe" -f $progFilesFolder)
+            $msdLocToCheck += ("{0}\IIS\Microsoft Web Deploy V2\msdeploy.exe" -f $progFilesFolder)
+            $msdLocToCheck += ("{0}\IIS\Microsoft Web Deploy\msdeploy.exe" -f $progFilesFolder)
+           
+            foreach($locToCheck in $msdLocToCheck) {
+                "Looking for msdeploy.exe at [{0}]" -f $locToCheck | Write-Verbose | Out-Null
+                if(Test-Path $locToCheck) {
+                    $msdInstallLoc = $locToCheck
+                    break;
+                }
+            }        
+        }
+    
+        if(!$msdInstallLoc){
+            throw "Unable to find msdeploy.exe, please install it and try again"
+        }
+    
+        "Found msdeploy.exe at [{0}]" -f $msdInstallLoc | Write-Verbose | Out-Null
+
+        return $msdInstallLoc
+    }
+}
+
+function Get-MSDeployFullUrlFor{
+    [cmdletbinding()]
+    param($msdeployServiceUrl)
+    process{
+        # Convert sayedkdemo.scm.azurewebsites.net:443 to https://sayedkdemo.scm.azurewebsites.net/msdeploy.axd
+        # TODO: This needs to be improved, it only works with Azure Websites. We have code for this.
+        'https://{0}/msdeploy.axd' -f $msdeployServiceUrl.TrimEnd(':443')
+    }
+}
+
+
+Export-ModuleMember -function Get-*,AspNet-*
