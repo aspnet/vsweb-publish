886b281ee10fd18c454f2d87cfd4bd2a02992c4a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-11-04 09:21:42 -0800

    updating

--- samples/end-to-end-01.ps1
+++ samples/end-to-end-01.ps1
@@ -1,8 +1,8 @@
-﻿
-
-
-$pubTempDir = 'c:\temp\publishtemp\e2e01'
-$packOutDir = (Join-Path $pubTempDir 'packout')
+﻿[cmdletbinding()]
+param(
+    $pubTempDir = 'c:\temp\publishtemp\e2e01',
+    $packOutDir = (Join-Path $pubTempDir 'packout')
+)
 
 if(Test-Path $pubTempDir){ Remove-Item $pubTempDir -Recurse -Force }
 if(Test-Path $packOutDir){ Remove-Item $packOutDir -Recurse -Force }
@@ -17,27 +17,37 @@ Push-Location
 Set-Location $pubTempDir
 git clone git@github.com:ligershark/aspnet_vnext_samples.git
 
-Set-Location aspnet_vnext_samples\mvc
-
-'*********************************************
-restoring nuget packages
-*********************************************' | Write-Output
-kpm restore
-'*********************************************
-building project
-*********************************************' | Write-Output
-kpm build
-$outdir = (join-path $packOutDir 'mvc')
-new-item $outdir -ItemType Directory
-$outdir = (get-item $outdir).FullName
-
-'*********************************************
-packing to [{0}]
-*********************************************' -f $outdir | Write-Output
-
-kpm @('pack', '-o', "$outdir")
-Set-Location $outdir
-#&((get-item(join-path $outdir 'kestrel.cmd')).FullName)
-
 Pop-Location
+function BuildAndPack{
+    [cmdletbinding()]
+    param(
+        $rootSrcdir,
+        $name,
+        $packOutdir
+    )
+    begin{ Push-Location }
+    end{ Pop-Location }
+    process{
+        $srcDir = (get-item (join-path $rootSrcdir $name)).FullName
+        Set-Location $srcDir
+'*********************************************
+building and packing [{0}]
+*********************************************' -f $srcDir | Write-Output
+'***** restoring nuget packages ****' | Write-Output
+        kpm restore
 
+'**** building project ****' | Write-Output
+        kpm build
+
+'**** packing to [{0}] ****' -f $outdir | Write-Output
+        $outdir = (new-item (join-path $packOutDir $name) -ItemType Directory).FullName
+        kpm @('pack', '-o', "$outdir")
+        Set-Location $outdir
+    }
+}
+
+BuildAndPack -rootSrcdir "$pubTempDir\aspnet_vnext_samples" -name 'console' -packOutdir $packOutDir
+BuildAndPack -rootSrcdir "$pubTempDir\aspnet_vnext_samples" -name 'web' -packOutdir $packOutDir
+BuildAndPack -rootSrcdir "$pubTempDir\aspnet_vnext_samples" -name 'mvc' -packOutdir $packOutDir
+'starting'|Write-Host -ForegroundColor Green
+start "$pubTempDir"
