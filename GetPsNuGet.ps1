a50f92361ad158c40896924cb64928dd06a6242a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-11-08 13:00:04 -0800

    adding getpsnuget related to #2

---
+++ GetPsNuGet.ps1
@@ -0,0 +1,20 @@
+﻿<#
+    You should add the snippet below to your PS files to ensure that PsNuGet is available.
+#>
+function Ensure-PsNuGetLoaded{
+    [cmdletbinding()]
+    param($toolsDir = "$env:LOCALAPPDATA\LigerShark\psnuget\",
+        $psNuGetDownloadUrl = 'https://raw.githubusercontent.com/sayedihashimi/publish-module/master/ps-nuget.psm1')
+    process{
+        if(!(Test-Path $toolsDir)){ New-Item -Path $toolsDir -ItemType Directory }
+
+        $expectedPath = (Join-Path ($toolsDir) 'ps-nuget.psm1')
+        if(!(Test-Path $expectedPath)){
+            'Downloading [{0}] to [{1}]' -f $psNuGetDownloadUrl,$expectedPath | Write-Verbose
+            (New-Object System.Net.WebClient).DownloadFile($psNuGetDownloadUrl, $expectedPath)
+        }
+        
+        Remove-Module 'ps-nuget' -Force
+        Import-Module $expectedPath 
+    }
+}
