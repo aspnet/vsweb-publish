7445e22da0f6378b041eae10222398134db29b33
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-11-20 16:45:28 -0800

    build related updates

--- build.ps1
+++ build.ps1
@@ -1,5 +1,13 @@
-[cmdletbinding()]
-param()
+[cmdletbinding(DefaultParameterSetName ='build')]
+param(
+    [Parameter(Position=0)]
+    [ValidateSet('build','create-local-nuget-repo')]
+    [Parameter(ParameterSetName='build')]
+    [string]$action='build',
+
+    [Parameter(ParameterSetName='create-local-nuget-repo',Position=1)]
+    [bool]$updateNugetExe = $false
+)
 
 function Get-ScriptDirectory
 {
@@ -8,6 +16,10 @@ function Get-ScriptDirectory
 }
 
 $scriptDir = ((Get-ScriptDirectory) + "\")
+
+$global:publishmodbuildsettings = New-Object PSObject -Property @{
+    LocalNuGetFeedPath = ('{0}\LigerShark\publish-module\nugetfeed' -f $env:localappdata)
+}
 
 <#
 .SYNOPSIS
@@ -43,26 +55,89 @@ function Get-Nuget(){
     }
 }
 
-$outputRoot = Join-Path $scriptDir "OutputRoot"
-$nugetDevRepo = 'C:\temp\nuget\localrepo\'
+function DoBuild{
+    $outputRoot = Join-Path $scriptDir "OutputRoot"
+    $nugetDevRepo = 'C:\temp\nuget\localrepo\'
 
-if(!(Test-Path $outputRoot)){
-    New-Item $outputRoot -ItemType Directory
+    if(!(Test-Path $outputRoot)){
+        New-Item $outputRoot -ItemType Directory
+    }
+
+    $outputRoot = (Get-Item $outputRoot).FullName
+    # call nuget to create the package
+
+    $nuspecFiles = @((get-item(Join-Path $scriptDir "publish-module.nuspec")).FullName)
+    $nuspecFiles += (get-item(Join-Path $scriptDir "publish-module-blob.nuspec")).FullName
+    $nuspecFiles += (get-item(Join-Path $scriptDir "ps-nuget.nuspec")).FullName
+
+    $nuspecFiles | ForEach-Object {
+        $nugetArgs = @('pack',$_,'-o',$outputRoot)
+        'Calling nuget.exe with the command:[nuget.exe {0}]' -f  ($nugetArgs -join ' ') | Write-Verbose
+        &(Get-Nuget) $nugetArgs    
+    }
+
+    if(Test-Path $nugetDevRepo){
+        Get-ChildItem -Path $outputRoot '*.nupkg' | Copy-Item -Destination $nugetDevRepo
+    }
 }
 
-$outputRoot = (Get-Item $outputRoot).FullName
-# call nuget to create the package
+function GetFolderCreateIfNotExists{
+    [cmdletbinding()]
+    param(
+        [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)]
+        [string[]]$folderPath
+    )
+    process{
+        foreach($folder in $folderPath){
+            if(!(Test-Path $folderPath)){
+                New-Item -Path $folderPath -ItemType Directory | out-null
+            }
 
-$nuspecFiles = @((get-item(Join-Path $scriptDir "publish-module.nuspec")).FullName)
-$nuspecFiles += (get-item(Join-Path $scriptDir "publish-module-blob.nuspec")).FullName
-$nuspecFiles += (get-item(Join-Path $scriptDir "ps-nuget.nuspec")).FullName
-
-$nuspecFiles | ForEach-Object {
-    $nugetArgs = @('pack',$_,'-o',$outputRoot)
-    'Calling nuget.exe with the command:[nuget.exe {0}]' -f  ($nugetArgs -join ' ') | Write-Verbose
-    &(Get-Nuget) $nugetArgs    
+            (Get-Item $folderPath).FullName
+        }
+    }
 }
 
-if(Test-Path $nugetDevRepo){
-    Get-ChildItem -Path $outputRoot '*.nupkg' | Copy-Item -Destination $nugetDevRepo
+function CreateLocalNuGetRepo{
+    [cmdletbinding()]
+    param($updateNugetExe = $false)
+    process{
+        $nugetFolder = (GetFolderCreateIfNotExists $global:publishmodbuildsettings.LocalNuGetFeedPath)
+    
+        $pkgsConfigContent = @'
+<?xml version="1.0" encoding="utf-8"?>
+<packages>
+    <package id="publish-module" />
+    <package id="publish-module-blob" />
+</packages>
+'@
+        $pkgsConfigPath = ("$nugetFolder\packages.config")
+        if(Test-Path $pkgsConfigPath){
+            rm -r $pkgsConfigPath
+        }
+
+        $pkgsConfigContent | Set-Content -Path $pkgsConfigPath
+
+        if($updateNugetExe){
+            # update nuget.exe first
+            &(Get-NuGet) @('update','-self')
+            Copy-Item -Path (Get-Nuget) -Destination $nugetFolder
+        }
+        # copy nuget.exe over there and update it
+        $pkgsToInstall = @('publish-module','publish-module-blob')
+
+        # call nuget to restore the packages that we want
+        $nugetArgs = @('install',$pkgsConfigPath,'-o',$nugetFolder)
+        'Calling nuget.exe with the following [nuget.exe {0}]' -f ($nugetArgs -join ',') | Write-Verbose
+        &(Get-Nuget) $nugetArgs
+    }
 }
+
+# Begin script here
+
+switch($action){
+    'build' {DoBuild}
+    'create-local-nuget-repo' {CreateLocalNuGetRepo}
+    'default' {throw ('Unknown value for action: [{0}]' -f $action)}
+}
+
