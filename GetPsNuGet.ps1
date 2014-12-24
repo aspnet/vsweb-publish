4973f6ca29758fe386fe2d93d96f53c8422aa3b5
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-12-23 18:38:05 -0800

    updating to version 0.0.16-beta

--- GetPsNuGet.ps1
+++ GetPsNuGet.ps1
@@ -9,9 +9,9 @@ function Enable-PsNuGet{
         if(!(get-module 'getnuget')){            
             if(!(Test-Path $toolsDir)){ New-Item -Path $toolsDir -ItemType Directory | Out-Null }
 
-            $modPath = (join-path $toolsDir 'getnuget.0.0.15-beta\tools\getnuget.psm1')
+            $modPath = (join-path $toolsDir 'getnuget.0.0.16-beta\tools\getnuget.psm1')
             if(!(Test-Path $modPath)){
-                $nugetArgs = @('install','getnuget','-prerelease','-version','0.0.15-beta','-OutputDirectory',(Resolve-Path $toolsDir).ToString())
+                $nugetArgs = @('install','getnuget','-prerelease','-version','0.0.16-beta','-OutputDirectory',(Resolve-Path $toolsDir).ToString())
                 $nugetDestPath = Join-Path -Path $toolsDir -ChildPath nuget.exe
                 if(!(Test-Path $nugetDestPath)){ (New-Object System.Net.WebClient).DownloadFile($nugetDownloadUrl, $nugetDestPath) | Out-Null }
                 if(!(Test-Path $nugetDestPath)){ throw 'unable to download nuget' }
