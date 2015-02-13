bc88aaf00a0ad75515ccaae09b1a93eba11b44ce
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-02-12 18:39:34 -0800

    minor update to appveyor.ps1

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,6 +1,7 @@
 ﻿$env:ExitOnPesterFail = $true
 $env:IsDeveloperMachine=$true
 $env:PesterEnableCodeCoverage = $true
+$env:e2ePkgTestUseCustomMSDeploy=$true
 
 if($env:APPVEYOR_REPO_BRANCH -ne "release"){
     .\build.ps1 -build
