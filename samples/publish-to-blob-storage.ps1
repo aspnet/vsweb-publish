bd346403f630c6515849274b65aefe484426fded
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-03-17 20:27:49 -0700

    updating version

--- samples/publish-to-blob-storage.ps1
+++ samples/publish-to-blob-storage.ps1
@@ -3,7 +3,7 @@ param($publishProperties, $packOutput)
 
 function Ensure-PublishModuleLoaded{
     [cmdletbinding()]
-    param($versionToInstall = '1.0.1-beta2',
+    param($versionToInstall = '1.0.1-beta3',
         $installScriptUrl = 'http://go.microsoft.com/fwlink/?LinkId=524326', # GetPublishModule.ps1
         $toolsDir = ("$env:LOCALAPPDATA\Microsoft\Web Tools\Publish\tools\"),
         $installScriptPath = (Join-Path $toolsDir 'GetPublishModule.ps1'))
