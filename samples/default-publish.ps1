3f55114ba4f9d15188781b6b3895bc2200ef9876
Vijay Ramakrishnan <vramak@microsoft.com>
2016-05-16 10:42:41 -0700

    Updating the script version to 1.1.0

--- samples/default-publish.ps1
+++ samples/default-publish.ps1
@@ -5,7 +5,7 @@
 param($publishProperties=@{}, $packOutput,$pubProfilePath, $nugetUrl)
 
 # to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
-$publishModuleVersion = '1.0.2-beta2'
+$publishModuleVersion = '1.1.0'
 
 function Get-PublishModulePath{
     [cmdletbinding()]
